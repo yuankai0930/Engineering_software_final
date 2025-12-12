13c28c97f3b508f8d46862447ccbaa9ac2ab41cd
yuankai0930 <yuankai0930@users.noreply.github.com>
2025-12-12 09:51:14 +0800

    Initial commit: ASP.NET Core MVC web application with Razor Runtime Compilation

---
+++ work_webapp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace work_webapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
